5c079eb4345168f3d48af11e530f3cfc66a1f21b
Tozé Mandela <mandelajunior10@gmail.com>
2022-11-05 16:18:57 +0100

    instalando modulos necessários para o express

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
+require('dotenv').config();
+
+let path = require('path');
+
 let express = require('express');
 let app = express();
-let path = require('path');
-let PORT= 3000;
+const Router = require('./router');
+
+const MongoStore = require('connect-mongo');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTBD).then(()=>{
+    app.emit('is connected');
+    console.log('BD connect com sucesso...');
+}).catch((e)=>{ 
+    console.log('Erro in connect to BD', e);
+});
+
+const session = require('express-session');
+
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(session({
+    secret: process.env.SECRET,
+    Store: MongoStore.create({mongoUrl: process.env.CONNECTBD}),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 1,
+        httpOnly: true 
+    }
+}))
 
-app.get('/',(req, res)=>{
-    res.render('page/home');
+
+app.use('/', Router)
+
+app.on('is connected', ()=>{
+    app.listen(process.env.PORT, ()=>{
+        console.log('servidor askSuite rodando');
+    });
 })
-
-app.listen(PORT, ()=>{
-    console.log('servidor askSuite rodando');
-});
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './frontEnd/index.js',
     output: {
         path: path.resolve(__dirname, 'public', 'js'),
         filename: 'bundle.js'
